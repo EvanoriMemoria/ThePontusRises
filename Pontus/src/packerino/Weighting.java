5efcf61ea90704695d9b598edeb4d94b5b312318
FergusPrimus <fergusprimus@gmail.com>
2015-10-04 10:34:26 -0600

    Added in the majorImpEvent method for major impossible events.

--- Pontus/src/packerino/Weighting.java
+++ Pontus/src/packerino/Weighting.java
@@ -56,8 +56,7 @@ public class Weighting {
 	   
 	public static void stablerino(String inpu) {
 	    	   
-		if(inpu.equals("Stable"))
-		{
+		if(inpu.equals("Stable")) {
 			List<Weighting> stableWeightings = new ArrayList<Weighting>();
 			stableWeightings.add(new Weighting(0, 5));
 			stableWeightings.add(new Weighting(1, 11));
@@ -71,31 +70,25 @@ public class Weighting {
 			       
 			System.out.println("Stable Original: " + stableWeight);    	
 			       
-			if(stableWeight==0) 
-			{
+			if(stableWeight==0) {
 				System.out.println("Location is now Compromised! Rolling on the Compromised table.");
 			}
-			else if(stableWeight==1) 
-			{
+			else if(stableWeight==1) {
 				System.out.println("Disappearance!");
 			}
-			else if(stableWeight==2)
-			{
+			else if(stableWeight==2) {
 				System.out.println("Spawn Chimera");
 			}
-			else if(stableWeight==3)
-			{
+			else if(stableWeight==3) {
 				System.out.println("Spawn Aberation");
 			}
-			else if(stableWeight==4)
-			{
+			else if(stableWeight==4) {
 		   	   System.out.println("Impossible Event: Major");
 		    }
 			/*This if loop triggers when the first pontus event is strange weather, 
 			* there is a chance for a second pontus event.
 			*/
-			else if(stableWeight==5) 
-			{
+			else if(stableWeight==5) {
 		 	   System.out.println("Impossible Weather: Major");
 		   	   List<Weighting> stableTeirTwo = new ArrayList<Weighting>();
 		   	   stableTeirTwo.add(new Weighting(2, 9));
@@ -104,12 +97,10 @@ public class Weighting {
 			   stableTeirTwo.add(new Weighting(6, 20));
 			   System.out.println(weightedRandom(stableTeirTwo));
 			}
-			else if(stableWeight==6)
-			{
+			else if(stableWeight==6) {
 				System.out.println("Nothing happens!");
 			}
-			else
-			{
+			else{
 				System.out.println("Something is very very wrong here...");
 			}
 			
@@ -132,13 +123,17 @@ public class Weighting {
 		    		   + "assumed to emerge at some random location on the Earth or in\n"
 		    		   + "the Borderlands. Other characters and objects are swallowed by\n"
 		    		   + "the Pontus and may eventually emerge as part of a chimera.\n");
+		}	
+	}
+	public static void majorImpEvent() {
+		Random randomGenerator = new Random();
+		int randomInt = randomGenerator.nextInt(100);
+		if(randomInt == 1) {
+			System.out.println("Momentum stops, everything instantly loses its momentum, including bullets.");
 		}
-		
-		List<Weighting> impossibleEvents = new ArrayList<Weighting>();
-			impossibleEvents.add(new Weighting(2, 9));
-			impossibleEvents.add(new Weighting(3, 9));
-			impossibleEvents.add(new Weighting(4, 9));
-			impossibleEvents.add(new Weighting(6, 20));
+		else if(randomInt == 2) {
+			
+		}
 	}
 }
 
